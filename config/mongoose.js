e27cf91b35e160d0410d1e28fa0f04d58673efdc
shiva <shivacheripally14@gmail.com>
2023-04-12 19:33:58 +0530

    ejs installed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 //require the mongoose library
 const mongoose = require('mongoose');
 //connnect to db
-mongoose.connect('mongodb://127.0.0.1:27017/Todo_list_db');
+mongoose.connect('mongodb+srv://shivacheripally14:s4fkxGKq9XaXzelg@cluster0.evidqem.mongodb.net/?retryWrites=true&w=majority');
 //aquire the connection
 const db = mongoose.connection;
 //if Error
@@ -10,3 +10,7 @@ db.once('error',console.error.bind(console,'Error connecting to db'));
 db.once('open',function(){
     console.log('Successfully Connected to DataBase');
 });
+
+// s4fkxGKq9XaXzelg
+
+// mongodb+srv://shivacheripally14:<password>@cluster0.evidqem.mongodb.net/?retryWrites=true&w=majority
